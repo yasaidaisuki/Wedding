ba927f4757e840cf2fc21c0a2d727e7403e71752
Zoe Peng <zy2peng@outlook.com>
2023-02-26 01:36:57 -0500

    * updating footer css * removed useless post

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,9 +6,10 @@ const Footer = () => {
 		<footer className="bg-neutral-50 border-t border-neutral-200">
 			<Container>
 				<div className="py-28 flex flex-col lg:flex-row items-center">
-					<h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-						Join us on <br />
-						September 6 2023
+					<h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-right mb-10 lg:mb-0 lg:pr-4 lg:w-full">
+						Join us on<br />
+						September 6<br />
+						2023
 					</h3>
 				</div>
 			</Container>
